aba55ae82656f08a5101851df198578c1cec7556
luhuadong <luhuadong@163.com>
2024-01-21 23:31:38 +0800

    update ROS2 (foxy) demo

--- project/ros2_demo/src/my_node.cpp
+++ project/ros2_demo/src/my_node.cpp
@@ -1,5 +1,12 @@
+#include <chrono>
+#include <functional>
+#include <memory>
+#include <string>
+
 #include <rclcpp/rclcpp.hpp>
 #include <std_msgs/msg/string.hpp>
+
+using namespace std::chrono_literals;
 
 class MyNode : public rclcpp::Node
 {
@@ -8,24 +15,30 @@ public:
         : Node(name)
     {
         publisher_ = create_publisher<std_msgs::msg::String>("/my_topic", 10);
+        timer_ = this->create_wall_timer(1000ms, std::bind(&MyNode::timer_callback, this));
+
         subscription_ = create_subscription<std_msgs::msg::String>(
-            "/my_topic", 10, std::bind(&MyNode::callback, this, std::placeholders::_1));
+            "/my_topic", 10, std::bind(&MyNode::sub_callback, this, std::placeholders::_1));
     }
 
 private:
-    void callback(const std_msgs::msg::String::SharedPtr msg)
+    void timer_callback()
+    {
+      auto message = std_msgs::msg::String();
+      message.data = "Hello, world! " + std::to_string(count_++);
+      RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
+      publisher_->publish(message);
+    }
+
+    void sub_callback(const std_msgs::msg::String::SharedPtr msg)
     {
         RCLCPP_INFO(get_logger(), "Received: %s", msg->data.c_str());
-
-        // Modify the received message and publish it again
-        auto new_msg = std::make_shared<std_msgs::msg::String>();
-        new_msg->data = "Modified: " + msg->data;
-        publisher_->publish(*new_msg);
-        RCLCPP_INFO(get_logger(), "Published: %s", new_msg->data.c_str());
     }
 
     rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
     rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
+    rclcpp::TimerBase::SharedPtr timer_;
+    size_t count_;
 };
 
 int main(int argc, char ** argv)
@@ -33,6 +46,7 @@ int main(int argc, char ** argv)
     rclcpp::init (argc, argv);
 
     auto my_node = std::make_shared<MyNode>("my_node");
+
     rclcpp::spin(my_node);
     rclcpp::shutdown();
 
